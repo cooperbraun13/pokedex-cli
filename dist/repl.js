54ef1ec3a2751c8e332b71ee57ba6a8cd45a016d
cooperbraun13 <cooperjbraun13@gmail.com>
2025-10-22 00:53:45 -0700

    feat: finished basic repl. takes input into cli and returns first word/command

---
+++ dist/repl.js
@@ -0,0 +1,25 @@
+import { createInterface } from "readline";
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on("line", async (input) => {
+        const words = cleanInput(input);
+        if (words.length === 0) {
+            rl.prompt();
+        }
+        const commandName = words[0];
+        console.log(`Your command was: ${commandName}`);
+        rl.prompt();
+    });
+}
+export function cleanInput(input) {
+    return input
+        .toLowerCase()
+        .trim()
+        .split(" ")
+        .filter((word) => word !== "");
+}
