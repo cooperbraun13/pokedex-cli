1e36a5010ddab3cd7fe4c3d90b019e197d0febd3
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-18 14:24:31 -0800

    feat: added new methods to delete any old entries in the cache, a cache cleanup loop, and a method to stop that interval loop when the cache is being shut down

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -7,6 +7,15 @@ export type CacheEntry<T> = {
 export class Cache {
   // map of key -> CacheEntry
   #cache = new Map<string, CacheEntry<any>>();
+  // timer ID for our cleanup loop (reaper)
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  // how long entries are allowed to live (in ms)
+  #interval: number;
+
+  constructor(interval: number) {
+    this.#interval = interval;
+    this.#startReapLoop();
+  }
 
   // add an entry to the cache
   add<T>(key: string, val: T): void {
@@ -22,5 +31,31 @@ export class Cache {
   get<T>(key: string): CacheEntry<T> | undefined {
     // if key does not exist, Map.get returns undefined automatically
     return this.#cache.get(key);
+  }
+
+  // delete any entries that are older than the interval
+  #reap(): void {
+    const now = Date.now();
+
+    for (const [key, entry] of this.#cache) {
+      if (now - entry.createdAt > this.#interval) {
+        this.#cache.delete(key);
+      }
+    }
+  }
+
+  // start a cleanup loop
+  #startReapLoop(): void {
+    this.#reapIntervalId = setInterval(() => {
+      this.#reap();
+    }, this.#interval);
+  }
+
+  // stop the interval loop when the cache is being shut down
+  stopReapLoop(): void {
+    if (this.#reapIntervalId !== undefined) {
+      clearInterval(this.#reapIntervalId);
+      this.#reapIntervalId = undefined;
+    }
   }
 }
